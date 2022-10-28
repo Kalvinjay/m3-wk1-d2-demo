71a280a0869c010d62e6db0078f72d4d0699c0a5
Kalvin Abraham-Mendez <kalvinjay@Kalvins-MacBook-Pro.local>
2022-10-27 22:17:41 -0500

    first commit

---
+++ wk1-day2-exercise/exp_demo3b.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+var routedemo = require('./exp_demo3a.js');
+
+
+//both exp-demo3a.js and exp-demo3b.js are in same directory
+app.use('./routes', routedemo);
+
+app.listen(3000);
